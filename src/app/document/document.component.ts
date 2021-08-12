c0944a4e9636db5dcbb0cc268da01fce86657e06
yaoh125 <yaoh125>
2021-08-12 16:44:27 +0900

    사용하지 않는 부분 주석처리

--- src/app/document/document.component.ts
+++ src/app/document/document.component.ts
@@ -4,14 +4,12 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {NGXLogger} from "ngx-logger";
 import {DocumentService} from "./document.service";
 import {DOCUMENTURLConstant} from "./common/document.constant";
-import {VERSION} from "@angular/core";
 import 'prismjs';
 import 'prismjs/components/prism-typescript.min.js';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
 import 'prismjs/plugins/line-highlight/prism-line-highlight.js';
 import "prismjs/components/prism-css.min.js";
 import {MarkdownModuleConfig, MarkdownService} from "ngx-markdown";
-import {Organization} from "../model/organization";
 
 
 declare var $: any;
@@ -56,7 +54,7 @@ export class DocumentComponent implements OnInit {
 
   ngOnInit() {
     this.doLayout()
-    this.doGetGuideList()
+    //this.doGetGuideList()
   }
 
   doLayout() {
@@ -69,80 +67,80 @@ export class DocumentComponent implements OnInit {
     });
   }
 
-  //등록된 가이드 리스트를 가져온다.
-  doGetGuideList() {
-    this.documentService.getGuides('/commonapi/v2/guides').subscribe(data => {
-      this.guides.push(data)
-      this.guidelist = this.guides['0']['data']
-      // for(var i = 0; i < this.guidelist.length; i++) {
-      //   Object.keys(this.guidelist).forEach(key=>{
-      //       const guidObj = this.guidelist[key];
-      //       this.guidelistname = guidObj['name']
-      //       this.guidelistsummary = guidObj['gubun']
-      //       this.guidelistdivision =guidObj['gubun2']
-      //       $("#guidelist").append('<tr id='+this.guidelistname+'><td> '+ this.guidelistname +' '+ this.guidelistsummary +' '+ this.guidelistdivision +' </td></tr>');
-      //   })
-      // }
-    }, error => {
-    });
-  }
-
-
-  // 가이드를 가져온다.
-  doGetGuide(event) {
-    const targetId = event.target.id
-    this.guidename = targetId
-    this.guideId = this.guidename.toString()
-    this.documentService.getGuide('/commonapi/v2/guides/' + this.guidename).subscribe(data => {
-      if (data['RESULT'] == DOCUMENTURLConstant.SUCCESS) {
-        this.division = data.data['gubun'];
-        this.summary = data.data['gubun2'];
-        this.markdown = data.data['markdown'];
-        this.getGuideImg()
-      } else {
-        this.errorMsg(this.translateEntities.document.guideFail);
-      }
-    })
-  }
-
-// 가이드 이미지를 불러온다.
-  getGuideImg() {
-    $("#guideImg").empty()
-    this.documentService.getGuide('/commonapi/v2/guide_images').subscribe(data => {
-      for (var i = 0; i < data['data'].length; i++){
-        if(this.summary == data['data'][i]['gubun2']){
-          this.imgno = data['data'][i]['id']
-          this.documentService.getGuide('/commonapi/v2/guide_images/' + this.imgno).subscribe(data => {
-            this.imgname = data.data.name
-            this.imgdivision = data.data['gubun']
-            this.imgpath = data.data['url']
-            this.imgsummary = data.data['gubun2']
-            var src = this.imgpath
-            $("#guideImg").append('<img src="'+src+'"/>') //나중에 아이콘으로 추가할 것
-          })
-        }
-      }
-    })
-  }
-
-
-  // 가이드 이미지를 리스트 불러온다.
-  getGuideImgs() {
-    this.documentService.getGuide('/commonapi/v2/guide_images').subscribe(data => {
-        if (data['RESULT'] == DOCUMENTURLConstant.SUCCESS) {
-          this.imgs = JSON.stringify(data)
-          this.guideimgs = JSON.parse(String(this.imgs))
-          this.imgform = this.guideimgs['data']
-        }
-      }
-    )
-  }
-
-  // 알럿 메시지(오류)
-  errorMsg(value: any) {
-    this.documentService.alertMessage(value, false);
-    this.documentService.isLoading(false);
-  }
+//   //등록된 가이드 리스트를 가져온다.
+//   doGetGuideList() {
+//     this.documentService.getGuides('/commonapi/v2/guides').subscribe(data => {
+//       this.guides.push(data)
+//       this.guidelist = this.guides['0']['data']
+//       // for(var i = 0; i < this.guidelist.length; i++) {
+//       //   Object.keys(this.guidelist).forEach(key=>{
+//       //       const guidObj = this.guidelist[key];
+//       //       this.guidelistname = guidObj['name']
+//       //       this.guidelistsummary = guidObj['gubun']
+//       //       this.guidelistdivision =guidObj['gubun2']
+//       //       $("#guidelist").append('<tr id='+this.guidelistname+'><td> '+ this.guidelistname +' '+ this.guidelistsummary +' '+ this.guidelistdivision +' </td></tr>');
+//       //   })
+//       // }
+//     }, error => {
+//     });
+//   }
+//
+//
+//   // 가이드를 가져온다.
+//   doGetGuide(event) {
+//     const targetId = event.target.id
+//     this.guidename = targetId
+//     this.guideId = this.guidename.toString()
+//     this.documentService.getGuide('/commonapi/v2/guides/' + this.guidename).subscribe(data => {
+//       if (data['RESULT'] == DOCUMENTURLConstant.SUCCESS) {
+//         this.division = data.data['gubun'];
+//         this.summary = data.data['gubun2'];
+//         this.markdown = data.data['markdown'];
+//         this.getGuideImg()
+//       } else {
+//         this.errorMsg(this.translateEntities.document.guideFail);
+//       }
+//     })
+//   }
+//
+// // 가이드 이미지를 불러온다.
+//   getGuideImg() {
+//     $("#guideImg").empty()
+//     this.documentService.getGuide('/commonapi/v2/guide_images').subscribe(data => {
+//       for (var i = 0; i < data['data'].length; i++){
+//         if(this.summary == data['data'][i]['gubun2']){
+//           this.imgno = data['data'][i]['id']
+//           this.documentService.getGuide('/commonapi/v2/guide_images/' + this.imgno).subscribe(data => {
+//             this.imgname = data.data.name
+//             this.imgdivision = data.data['gubun']
+//             this.imgpath = data.data['url']
+//             this.imgsummary = data.data['gubun2']
+//             var src = this.imgpath
+//             $("#guideImg").append('<img src="'+src+'"/>') //나중에 아이콘으로 추가할 것
+//           })
+//         }
+//       }
+//     })
+//   }
+//
+//
+//   // 가이드 이미지를 리스트 불러온다.
+//   getGuideImgs() {
+//     this.documentService.getGuide('/commonapi/v2/guide_images').subscribe(data => {
+//         if (data['RESULT'] == DOCUMENTURLConstant.SUCCESS) {
+//           this.imgs = JSON.stringify(data)
+//           this.guideimgs = JSON.parse(String(this.imgs))
+//           this.imgform = this.guideimgs['data']
+//         }
+//       }
+//     )
+//   }
+//
+//   // 알럿 메시지(오류)
+//   errorMsg(value: any) {
+//     this.documentService.alertMessage(value, false);
+//     this.documentService.isLoading(false);
+//   }
 
 }
 
